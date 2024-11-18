14405e88063566df0263819e600e12085f6e8b86
StrendleyW <naufalrf0@gmail.com>
2024-11-18 12:59:42 +0700

    Last Commit

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,8 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require("@tensorflow/tfjs-node");
+const MODEL_URL =
+  "https://storage.googleapis.com/ai-model-cancer/submissions-model/model.json";
+
 async function loadModel() {
-    return tf.loadGraphModel(process.env.MODEL_URL);
+  return tf.loadGraphModel(process.env.MODEL_URL);
 }
 module.exports = loadModel;
